8227bbcdc6f758c6ceb1e5354b17a718fc158e40
longtian1911 <778707060@qq.com>
2018-06-20 17:17:22 +0800

    工厂方法模式

--- factoryFunc.php
+++ factoryFunc.php
@@ -5,5 +5,38 @@ interface Tell {
 }
 
 class XiaoMi implements Tell{
-	
+	public function call(){
+		echo "我在使用小米手机打电话";
+	}
+
+	public function receive(){
+		echo "我在使用小米手机接电话";
+	}
+}
+
+class HuaWei implements Tell {
+	public function call(){
+		echo "我在使用华为手机打电话";
+	}
+
+	public function receive(){
+		echo "我在使用华为手机接电话";
+	}
+}
+
+//工厂类是负责规定接口，具体的实现交给子类
+interface Factory {
+	static function createPhone();
+}
+
+class XiaoFactory implements Factory{
+	static function createPhone(){
+		return new XiaoMi();
+	}
+}
+
+class HuaWeiFactory implements Factory{
+	static function createPhone(){
+		return new HuaWei();
+	}
 }
