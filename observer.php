52a23013eec575a5e6614f9a0c13868486b71925
longtian1911 <778707060@qq.com>
2018-06-21 08:09:45 +0800

    观察者模式完成

--- observer.php
+++ observer.php
@@ -1,5 +1,5 @@
 <?php
-//观察者模式设计到两个来
+//观察者模式涉及到两个类
 //男人类和女朋友类
 //男人类对象小明，  小花、小丽
 class Man {
@@ -16,6 +16,14 @@ class Man {
 		foreach ($this->observers as $girl) {
 			$girl->dongjie();
 		}
+	}
+
+	//删除观察者方法
+	public function delObserver($observer){
+		//查找对于值在数组中的键，array_search 函数在数组中搜索某个键值，并返回对应的键名。
+		$key = array_search($observer, $this->observers);
+		//根据键删除之，并且数组重新索引，array_splice() 函数从数组中移除选定的元素，并用新元素取代它。该函数也将返回包含被移除元素的数组。
+		array_splice($this->observers, $key, 1);
 	}
 }
 
@@ -34,4 +42,5 @@ $xiaoli = new GirlFriend();
 $xiaoming->addObserver($xiaohua);
 $xiaoming->addObserver($xiaoli);
 
+$xiaoming->delObserver($xiaohua);
 $xiaoming->buy();
